9b0afb204c972d444593c421bd7503c6cde06747
terrill-chalmers <terrill-chalmers@protonmail.com>
2019-08-05 21:32:48 -0500

    feat: minimumSwaps with selectionSort 28/40

--- 1_arrays/minimum-swaps-2/minimumSwaps.js
+++ 1_arrays/minimum-swaps-2/minimumSwaps.js
@@ -2,47 +2,23 @@
 function minimumSwaps(arr) {
   let swapQty = 0;
 
-  const swap = (currentArray, startingLowIndex, startingHighIndex) => {
-    if (startingLowIndex !== startingHighIndex) {
-      swapQty += 1;
-    }
+  const swap = (currentArray, lowIndex, highIndex) => {
+    const temp = currentArray[lowIndex];
+    currentArray[lowIndex] = currentArray[highIndex];
+    currentArray[highIndex] = temp;
 
-    const highValue = currentArray[startingLowIndex];
-    currentArray[startingLowIndex] = currentArray[startingHighIndex];
-    currentArray[startingHighIndex] = highValue;
+    swapQty += 1;
   };
 
-  const quickSort = (currentArray, startingLowIndex, startingHighIndex) => {
-    let pivot = currentArray[Math.floor((startingHighIndex + startingLowIndex) / 2)];
-    let currentLowIndex = startingLowIndex;
-    let currentHighIndex = startingHighIndex;
-
-    while (currentLowIndex <= currentHighIndex) {
-      while (currentArray[currentLowIndex] < pivot) {
-        currentLowIndex += 1;
-      }
-      while (currentArray[currentHighIndex] > pivot) {
-        currentHighIndex -= 1;
-      }
-      if (currentLowIndex <= currentHighIndex) {
-        swap(currentArray, currentLowIndex, currentHighIndex);
-        currentLowIndex += 1;
-        currentHighIndex -= 1;
+  const selectionSort = currentArray => {
+    for (let index = 0; index < currentArray.length; index++) {
+      if (currentArray[index] !== index + 1) {
+        swap(currentArray, index, currentArray.indexOf(index + 1));
       }
     }
-
-    if (currentArray.length > 1) {
-      if (startingLowIndex < currentLowIndex - 1) {
-        quickSort(currentArray, startingLowIndex, currentLowIndex - 1);
-      }
-      if (currentLowIndex < startingHighIndex) {
-        quickSort(currentArray, currentLowIndex, startingHighIndex);
-      }
-    }
-    return currentArray;
   };
 
-  quickSort(arr, 0, arr.length - 1);
+  selectionSort(arr);
 
   return swapQty;
 }
